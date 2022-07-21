fba40599503fc24d34b8955dca7e808deb27ad3e
Alex <alex8224@mail.ru>
2022-07-21 18:28:47 +0300

    Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -3,39 +3,6 @@
 
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125
-int FirstFunction(string message)
-{
-    int result = 0;
-    string error = "Введите корректное число, пожалуйста!";
-
-    while(true)
-    {
-        Console.WriteLine(message);
-
-        if(int.TryParse(Console.ReadLine(), out result))
-        {
-           break;
-        }
-        else 
-        {
-            Console.Clear();
-            Console.WriteLine(error);
-        }
-    }
-
-    return result;
-}
-
-void Sqrt(int number)
-{
-    for (int i = 1; i < number; i++)
-    {
-        Console.WriteLine(Math.Pow(i,2));
-    }
-}
-
-int number = FirstFunction("Введите число: ");
-Sqrt(number);
 */
 int FirstFunction(string message)
 {
@@ -62,7 +29,7 @@ int FirstFunction(string message)
 
 void Sqrt(int number)
 {
-    for(int i = 1; i < number; i++)
+    for(int i = 1; i <= number; i++)
     {
         Console.WriteLine(Math.Pow(i, 3));
     }
